de18d1373fafa9d06de127e9139ff870837d3345
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-08-29 17:24:13 -0300

    req 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,70 @@
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup: {
+      from: "air_routes",
+      let: {
+        airlineFromAirRoutes: "$airlines",
+      },
+      pipeline: [
+        {
+          $match: {
+            $or: [
+              {
+                airplane: "747",
+              },
+              {
+                airplane: "380",
+              },
+            ],
+
+          },
+        },
+        {
+          $match: {
+            $expr: {
+              $and: [
+                {
+                  $eq: [
+                    "$airline.name",
+                    "$$airlineFromAirRoutes",
+                  ],
+                },
+              ],
+            },
+          },
+        },
+      ],
+      as: "airplaneAliances",
+    },
+  },
+  {
+    $match: {
+      airplaneAliances: {
+        $not: {
+          $size: 0,
+        },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: {
+        $sum: {
+          $size: "$airplaneAliances",
+        },
+      },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
